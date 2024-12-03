e98ec7c0c748c8b68ca946848d3bd59036a55318
cocoyuy <xy2141@nyu.edu>
2024-12-03 17:03:02 -0500

    updated tests

--- tests/all_tests.c
+++ tests/all_tests.c
@@ -24,54 +24,54 @@ void tearDown(void)
 int main(void)
 {
     // profiling
-    // profile_linear(SMALL_MAT, SMALL_MAT);
-    // profile_matmul(SMALL_MAT);
+    profile_linear(SMALL_MAT, SMALL_MAT);
+    profile_matmul(SMALL_MAT);
     profile_matmul(MEDIUM_MAT);
-    // profile_matmul(LARGE_MAT);
-    // profile_conv(SMALL_MAT);
-    // profile_softmax_small_input();
-    // profile_softmax_medium_input();
-    // profile_softmax_large_input();
-    // profile_relu_small_input();
-    // profile_relu_medium_input();
-    // profile_relu_large_input();
+    profile_matmul(LARGE_MAT);
+    profile_conv(SMALL_MAT);
+    profile_softmax_small_input();
+    profile_softmax_medium_input();
+    profile_softmax_large_input();
+    profile_relu_small_input();
+    profile_relu_medium_input();
+    profile_relu_large_input();
 
     // tests
 
-    // UNITY_BEGIN();
+    UNITY_BEGIN();
 
     // Test conv
-    // RUN_TEST(test_conv);
-    // RUN_TEST(test_conv_minimal);
-    // RUN_TEST(test_conv_multiple_filters_channels);
+    RUN_TEST(test_conv);
+    RUN_TEST(test_conv_minimal);
+    RUN_TEST(test_conv_multiple_filters_channels);
 
     // Test nn
-    // RUN_TEST(test_flatten_basic);
-    // RUN_TEST(test_predict_simple_array);
-    // RUN_TEST(test_predict_all_same_values);
-    // RUN_TEST(test_predict_mix_of_negatives_and_positives);
+    RUN_TEST(test_flatten_basic);
+    RUN_TEST(test_predict_simple_array);
+    RUN_TEST(test_predict_all_same_values);
+    RUN_TEST(test_predict_mix_of_negatives_and_positives);
 
     // Test functional
-    // RUN_TEST(test_softmax_basic);
-    // RUN_TEST(test_relu);
+    RUN_TEST(test_softmax_basic);
+    RUN_TEST(test_relu);
 
     // Test linear
-    // RUN_TEST(test_linear_basic);
-    // RUN_TEST(test_linear_basic2);
-    // RUN_TEST(test_linear_minimal);
-    // RUN_TEST(test_linear_input_all_zeros);
-    // RUN_TEST(test_linear_weight_all_zeros);
-    // RUN_TEST(test_linear_medium_input);
-    // RUN_TEST(test_linear_large_input);
+    RUN_TEST(test_linear_basic);
+    RUN_TEST(test_linear_basic2);
+    RUN_TEST(test_linear_minimal);
+    RUN_TEST(test_linear_input_all_zeros);
+    RUN_TEST(test_linear_weight_all_zeros);
+    RUN_TEST(test_linear_medium_input);
+    RUN_TEST(test_linear_large_input);
 
     // Test matrix_ops
-    // RUN_TEST(test_matmul_square_matrices);
-    // RUN_TEST(test_matmul_incompatible_dimensions);
-    // RUN_TEST(test_matmul_minimal);
-    // RUN_TEST(test_matmul_with_zeros);
-    // RUN_TEST(test_matmul_with_negatives);
+    RUN_TEST(test_matmul_square_matrices);
+    RUN_TEST(test_matmul_incompatible_dimensions);
+    RUN_TEST(test_matmul_minimal);
+    RUN_TEST(test_matmul_with_zeros);
+    RUN_TEST(test_matmul_with_negatives);
 
-    // RUN_TEST(test_scaled_dot_product_attention);
+    RUN_TEST(test_scaled_dot_product_attention);
 
-    // return UNITY_END();
+    return UNITY_END();
 }
